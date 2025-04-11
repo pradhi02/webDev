e77a1ec03332c0685c6dc66c94af4e9a06f391db
Pradhi Rajak <95995251+pradhi02@users.noreply.github.com>
2025-04-11 19:01:40 +0530

    Add files via upload

---
+++ video54/script.js
@@ -0,0 +1,6 @@
+
+alert("hello there, what's sup"); 
+console.log("the code is running"); /*with or without semicolan both the cases it will run*/ 
+console.log("this code is also running") 
+var a=prompt("Enter your age") 
+console.log("your age is"+a)
