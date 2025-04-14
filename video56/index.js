bd39492635f8aa908d65500975bc5297b473b978
Pradhi Rajak <95995251+pradhi02@users.noreply.github.com>
2025-04-14 21:25:30 +0530

    Add files via upload

---
+++ video56/index.js
@@ -0,0 +1,17 @@
+console.log("hey, i will give you  conditional tutorial today")
+console.log("hey whats up how are you")
+let a={
+    name:"pradhi", 
+    age:83 , 
+    salary:1222
+} 
+console.log(a) 
+
+ let b=4; 
+
+ if(b>18){
+    console.log("you can drive")
+ }
+  else{
+    console.log("you cannot drive")
+  } 
